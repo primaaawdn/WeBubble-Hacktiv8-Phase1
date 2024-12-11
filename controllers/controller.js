513493edde60b0ce316c8cfee1b633d8c064f573
Prima <widianiprima@gmail.com>
2024-12-11 16:21:55 +0700

    feat: enhance login functionality with SweetAlert2

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,58 +1,58 @@
 const { User } = require("../models");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 class Controller {
-    static async landing(req, res) {
-        try {
-            res.render("Landing");
-        } catch (error) {
-            res.send(error.message);
-        }
-    }
-    static async loginPage(req, res) {
-        try {
-            res.render('Login');
-        } catch (error) {
-            res.send(error.message);
-        }  
-    }
-    static async login(req, res) {
-        try {
-            const { username, password } = req.body;
-            const user = await User.findOne({ where: { username } }); 
+	static async landing(req, res) {
+		try {
+			res.render("Landing");
+		} catch (error) {
+			res.send(error.message);
+		}
+	}
+	static async loginPage(req, res) {
+		try {
+			res.render("Login");
+		} catch (error) {
+			res.send(error.message);
+		}
+	}
+	static async login(req, res) {
+		try {
+			const { username, password } = req.body;
+			const user = await User.findOne({ where: { username } });
 
-            if (!user) {
-                return res.status(401).send("Invalid username or password.");
-            }
+			if (!user) {
+				return res.render('Login', { error: 'Invalid username' });
+			}
 
-            const passwordMatch = await bcrypt.compare(password, user.password);
+			const passwordMatch = await bcrypt.compare(password, user.password);
 
-            if (!passwordMatch) {
-                return res.status(401).send("Invalid username or password.");
-            }
-            req.session.userId = user.id;
-            res.redirect('/posts');
-        } catch (error) {
-            res.send(error.message);
-            if(error.name === "SequelizeValidationError"){
-                let errors = error.errors.map(e => e.message)
-                res.send(errors)
-              }
-        }
-    }
-    static logout(req, res) {
-        if (req.session) {
-            req.session.destroy(error => {
-                if (error) {
-                    console.error("Session destroy error:", error);
-                    return res.status(500).send("Error logging out.");
-                }
-                res.redirect('/');
-            });
-        } else {
-            res.redirect('/'); 
-        }
-    }
+			if (!passwordMatch) {
+				return res.render('Login', { error: 'Invalid password' });
+			}
+			req.session.userId = user.id;
+			res.render("Login", { success: true });
+		} catch (error) {
+			res.send(error.message);
+			if (error.name === "SequelizeValidationError") {
+				let errors = error.errors.map((e) => e.message);
+				res.send(errors);
+			}
+		}
+	}
+	static logout(req, res) {
+		if (req.session) {
+			req.session.destroy((error) => {
+				if (error) {
+					console.error("Session destroy error:", error);
+					return res.status(500).send("Error logging out.");
+				}
+				res.redirect("/");
+			});
+		} else {
+			res.redirect("/");
+		}
+	}
 }
 
 module.exports = Controller;
